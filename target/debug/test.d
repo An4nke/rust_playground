9c6b9bb42f499d1c352163b1830fe28c43c08b89
Clara Benedicta Maria Müller <clara.muellr@gmail.com>
2017-04-12 15:54:46 +0200

    first programm in rust

---
+++ target/debug/test.d
@@ -0,0 +1,1 @@
+/media/win_e/Clara_Daten/etc/Programmierung/Rust/src/target/debug/test: /media/win_e/Clara_Daten/etc/Programmierung/Rust/src/test.rs
---
+++ test.rs
@@ -0,0 +1,9 @@
+// my first rust programm
+// aka "rust playground"
+
+
+// main function
+fn main() {
+    println!("Hello world!"); //print to console -> important: here not a tab, but 4 spaces!
+}
+
